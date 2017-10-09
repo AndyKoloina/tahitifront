13e777832d9e20603b124220ffeabda74009617a
Andy Ranaivo Nirina Nantenaina <ranaivon@proximity.bbdo.fr>
2017-10-09 08:07:38 +0400

    edit map

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import { Slick } from 'slick-carousel';
 import bootstrap from 'bootstrap';
+import AOS from 'aos';
+AOS.init();
 import '../js/smooth-scroll';
 import '../js/hamburger';
 import '../js/hamburger-sidebar';
 import '../js/slick';
 import '../js/map';
+
 
--- src/js/map.js
+++ src/js/map.js
@@ -1,16 +1,61 @@
 (function () {
   'use strict';
   let marker = $('.marker');
+  let nav_link = $('.nav-link');
   let map_title = $('.map-title');
   let map_place = $('.map-place');
+
   jQuery.each(marker, function (i, val) {
     $(this).on('click', (e) => {
       let place = $(this).attr('id');
       displaydata(place);
       e.preventDefault();
-      $('.marker').removeClass('is-active-marker') / $(this).addClass('is-active-marker')
+      $('.marker').removeClass('is-active-marker');
+      $(this).addClass('is-active-marker')
     })
   });
+ 
+  /*jQuery.each(nav_link, function (i, val) {
+    let active = $(this).hasClass('active') ? true : false;
+      
+      /*let href = $(this).attr('href');
+      alert(href);
+      initmarker(href);
+       
+  });*/
+  $('.nav-tabs > li > a').on("click",function(e){
+      e.preventDefault();
+      let href = $(this).attr('href');
+     
+
+      initmarker(href);
+  });
+
+  function initmarker(href) {
+
+    if(href === '#Tahiti') {
+      $('.marker').removeClass('is-active-marker');
+      $('#Facebook_Places').addClass('is-active-marker');
+      map_title.html('Face tony');
+      map_place.html('Face au McDonald de Papeete');
+    } else if (href === '#Moorea') {
+      $('.marker').removeClass('is-active-marker');
+      $('#paopao').addClass('is-active-marker');
+      map_title.html('Papao');
+      map_place.html('lorem ipsum dorote vvsdfssd');
+    } else if (href === '#BoraBora') {
+      $('.marker').removeClass('is-active-marker');
+      $('#nunue').addClass('is-active-marker');
+      map_title.html('nunue');
+      map_place.html('lorem ipsum dorote vvsdfsf');
+    } else if (href === '#Raiatea') {
+      $('.marker').removeClass('is-active-marker');
+      $('#uturoa').addClass('is-active-marker');
+      map_title.html('uturoa');
+      map_place.html('lorem ipsum dorote vvsdfsf');
+    }
+    
+  }
   function displaydata(place) {
     if (place === 'Prince-hinoi') {
       map_title.html('Prince Hinoi');
