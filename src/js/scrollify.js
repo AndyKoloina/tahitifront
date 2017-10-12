4637abd5999373cc3834740c1afc21539df46c11
Andy Ranaivo Nirina Nantenaina <ranaivon@proximity.bbdo.fr>
2017-10-12 15:38:30 +0400

    add scrollify and fix desktop issue and mobile

---
+++ src/js/scrollify.js
@@ -0,0 +1,16 @@
+$(function() {
+  $.scrollify({
+    section : ".tahiti-section",
+    sectionName : "section-name",
+    interstitialSection : "header",
+    easing: "easeOutExpo",
+    scrollSpeed: 1000,
+    offset : 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: false,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+  });
+});
